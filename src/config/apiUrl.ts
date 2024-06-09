140b5e0f1eed5723fbb0e8006f0315428e0c63d1
arifaeva <arifa.eva.ixa@gmail.com>
2024-06-09 12:13:19 +0700

    update apiUrl

--- src/config/apiUrl.ts
+++ src/config/apiUrl.ts
@@ -1,5 +1,5 @@
 export const API_URL =
   process.env.NODE_ENV === "production"
-    ? "http://localhost:3000"
-    : "http://localhost:3000";
+    ? "http://103.74.5.212:5000"
+    : "http://localhost:5000";
 // export const API_URL = process.env.NODE_ENV === "production" ? "domain" : "http://localhost:8000";
